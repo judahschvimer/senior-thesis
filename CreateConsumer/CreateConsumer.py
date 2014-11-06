5b6958b8c727c4353293c314efd1921e3a0b1d1a
Judah Schvimer <jaschvimer@gmail.com>
2014-11-05 23:30:29 -0500

    finished initial python script

--- CreateConsumer/CreateConsumer.py
+++ CreateConsumer/CreateConsumer.py
@@ -1,34 +1,24 @@
 import sys
 
-def write_header(f, discount, num_prices, values, wtps, prices):
-    f.write('discount = {0}\n'.format(discount))
-    f.write('values = {0}\n'.format(values))
-    f.write('states: {0} done\n'.format(' '.join(wtps)))
-    f.write('actions: {0}\n'.format(' '.join(prices)))
-    f.write('observations: o-1 o-2\n')
-    f.write('\n')
 
-    f.write('start include: {0}\n'.format(' '.join(wtps)))
-    f.write('\n')
+def print_transition(f, action, start_state, end_state, probability):
+    f.write('T : {0} : {1} : {2} {3}\n'.format(action, start_state, end_state, probability))
 
-def write_transition(action, start_state, end_state, probability):
-    f.write('T : {0} : {1} : {2} {3}'.format(action, start_state, end_state, probability)
+def print_observation(f, action, end_state, observation, probability):
+    f.write('O : {0} : {1} : {2} {3}\n'.format(action, end_state, observation, probability))
 
-def write_observation(action, end_state, observation, probability):
-    f.write('O : {0} : {1} : {2} {3}'.format(action, end_state, observation, probability)
+def print_reward(f, action, start_state, end_state, observation, probability):
+    f.write('R : {0} : {1} : {2} : {3} {4}\n'.format(action, start_state, end_state, observation, probability))
 
-def write_result(action, start_state, end_state, observation, probability):
-    f.write('R : {0} : {1} : {2} : {3} {4}'.format(action, start_state, end_state, observation, probability)
+def wtp(p):
+    return 'wtp-' + str(p)
 
-def write_transitions(f, wtps, prices):
-    f.write('T : * : done : done : 1.0')
-    f.write('\n')
-
-
+def price(p):
+    return 'p-' + str(p)
 
 # Inputs: a willingness to pay and a price
 # Outputs: probability of leaving given the wtp and price
-def leave_function(wtp, price):
+def leave_function(price, wtp):
     leave_probability = 0.2
 
     if price <= wtp:
@@ -36,20 +26,57 @@ def leave_function(wtp, price):
     else:
         return leave_probability
 
-def write_pomdp(out_file_name, discount, num_prices, values, wtps, prices):
+def write_header(f, discount, num_prices, values, prices, wtps):
+    f.write('discount: {0}\n'.format(discount))
+    f.write('values: {0}\n'.format(values))
+    f.write('states: {0} done\n'.format(' '.join([wtp(p) for p in wtps])))
+    f.write('actions: {0}\n'.format(' '.join([price(p) for p in prices])))
+    f.write('observations: o-1 o-2\n')
+    f.write('\n')
+
+    f.write('start include: {0}\n'.format(' '.join([wtp(p) for p in wtps])))
+    f.write('\n')
+
+def write_transitions(f, prices, wtps):
+    print_transition(f, '*', 'done', 'done', 1.0)
+    f.write('\n')
+    for p in prices:
+        for w in wtps:
+            if p <= w:
+                print_transition(f, price(p), wtp(w), 'done', 1.0)
+            else:
+                leave_probability = leave_function(p, w)
+                print_transition(f, price(p), wtp(w), wtp(w), 1.0 - leave_probability)
+                print_transition(f, price(p), wtp(w), 'done', leave_probability)
+        f.write('\n')
+
+def write_observations(f, prices, wtps):
+    print_observation(f, '*', '*', 'o-1', 1.0)
+    f.write('\n')
+
+def write_rewards(f, prices, wtps):
+    for p in prices:
+        for w in wtps:
+            if p <= w:
+                print_reward(f, price(p), wtp(w), 'done', '*', float(p))
+        f.write('\n')
+
+def write_pomdp(out_file_name, discount, num_prices, values, prices, wtps):
     with open(out_file_name, 'w') as f:
-        write_header(f, discount, num_prices, values, wtps, prices)
-        write_transitions(f, wtps, prices)
+        write_header(f, discount, num_prices, values, prices, wtps)
+        write_transitions(f, prices, wtps)
+        write_observations(f, prices, wtps)
+        write_rewards(f, prices, wtps)
 
 def main():
     out_file_name = 'SolveConsumer.POMDP'
     discount = 0.95
     num_prices = 5
     values = 'reward'
-    wtps = ['wtp-' + str(s) for s in range(1, num_prices + 1)]
-    prices = ['p-' + str(s) for s in range(1, num_prices + 1)]
+    wtps = range(1, num_prices + 1)
+    prices = range(1, num_prices + 1)
 
-    write_pomdp(out_file_name, discount, num_prices, values, wtps, prices)
+    write_pomdp(out_file_name, discount, num_prices, values, prices, wtps)
 
 if __name__ == '__main__':
     main()
