14451b88f5dadf2455a25f572e94034c09b32d46
Judah Schvimer <jaschvimer@gmail.com>
2014-11-08 16:44:25 -0500

    fixed some bugs in grapher

--- CreateConsumer/GraphConsumer.py
+++ CreateConsumer/GraphConsumer.py
@@ -4,39 +4,49 @@ import os
 import numpy
 import matplotlib.pyplot as plt
 import subprocess
+from decimal import Decimal
+import itertools
+
 import CreateConsumer
 
 def get_max_starting_strategy(file_base, initial_belief_state):
-    with open(file_base + '.alpha', 'r') as f:
-        lines = f.readlines()
-        max_sum = -99999
-        max_strategy = -1
-        for idx, line in lines:
-            if idx % 3 == 2:
-                weights = map(int, line.split(' '))
-                sum = numpy.dot(weights, initial_belief_state)
-                if sum > max_sum
-                    max_strategy = (idx-2)/3
-    return max_strategy
+    for file_name in glob.glob('*.alpha'):
+        if file_name.startswith(file_base):
+            with open(file_name, 'r') as f:
+                lines = f.readlines()
+                max_sum = -99999
+                max_strategy = -1
+                for idx, line in enumerate(lines):
+                    line = line.strip()
+                    if (idx % 3) == 1:
+                        weights = map(Decimal, line.split())
+                        belief = map(Decimal, initial_belief_state)
+                        sum = numpy.dot(weights, belief)
+                        if sum > max_sum:
+                            max_sum = sum
+                            max_strategy = (idx-1)/3
+            return max_strategy
 
 def get_list_of_actions(file_base, start):
-    with open(file_base + '.pg', 'r') as f:
-        lines = f.readlines()
-        lines = map(split, lines)
-        curr = start
-        actions = []
-        bargaining = True
-        while bargaining:
-            actions.append(lines[curr][1] + 1)
-            curr = lines[curr][2]
-            if curr == 0:
-                bargaining = False
+    for file_name in glob.glob('*.pg'):
+        if file_name.startswith(file_base):
+            with open(file_name, 'r') as f:
+                lines = f.readlines()
+                lines = map(lambda(x): x.strip(), lines)
+                lines = map(lambda(x): x.split(), lines)
+                curr = start
+                actions = []
+                bargaining = True
+                while bargaining:
+                    actions.append(int(lines[curr][1]))
+                    curr = int(lines[curr][2])
+                    if curr == 0:
+                        bargaining = False
+                return actions
 
-def solve_pomdp(leave_probability):
+def solve_pomdp(leave_probability, num_prices):
     discount = 0.95
-    num_prices = 5
     values = 'reward'
-    leave_probability = 0.2
     file_base = 'p-{0}'.format(leave_probability)
     CreateConsumer.write_pomdp(file_base + '.POMDP', discount, num_prices, values, leave_probability)
     subprocess.call(['../pomdp-solve-5.3/src/pomdp-solve', '-pomdp', file_base + '.POMDP'])
@@ -56,19 +66,29 @@ def transform_list(strategy_list):
     return index_lists
 
 
-def parse_files(initial_belief_state):
+def parse_files(initial_belief_state, num_prices):
     file_strategies = {}
-    for p in range(0, 1, 1):
-        file_base = solve_pomdp(p)
-        start = get_max_starting_strategy(file_base initial_belief_state)
+    for p in frange(0, 1, .2):
+        file_base = solve_pomdp(p, num_prices)
+        start = get_max_starting_strategy(file_base, initial_belief_state)
         file_strategies[p] = get_list_of_actions(file_base, start)
     return file_strategies
 
+def frange(x, y, jump):
+  r = []
+  while x < y:
+    r.append(x)
+    x += jump
+  return r
+
 def main():
-    num_prices = 5
-    uniform_belief_state = range(0, 1, 1.0/num_prices)
-    file_strategies = parse_files(uniform_belief_state)
+    num_prices = 7
+    uniform_belief_state = [1.0/num_prices] * num_prices
+    uniform_belief_state.append(0)
+    file_strategies = parse_files(uniform_belief_state, num_prices)
+    print file_strategies
     graph_strategies = transform_list(file_strategies)
+    print graph_strategies
     marker = itertools.cycle((',', '+', '.', 'o', '*'))
     for i in range(0, len(graph_strategies)):
         data = zip(*(graph_strategies[i]))
